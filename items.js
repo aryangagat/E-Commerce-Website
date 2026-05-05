63243181b370cbd183fed5ca25a1e60e04382292
aryangagat <148423768+aryangagat@users.noreply.github.com>
2026-05-05 16:04:14 +0530

    Add items array with product details

---
+++ items.js
@@ -0,0 +1,159 @@
+const items = [
+    {
+        id: '001',
+        image: 'Top-image/Top1.avif',
+        company: 'VAN HEUSEN',
+        item_name: 'Women Brown Solid Polo-Neck Top',
+        original_price: 1999,
+        current_price: 699,
+        discount_percentage: 65,
+         return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
+    },
+     {
+        id: '002',
+        image: 'Top-image/Top5.jpg',
+        company: 'Tokyo Talkies',
+        item_name: 'Chill Oversized T-Shirt For Women',
+        original_price: 949,
+        current_price: 294,
+        discount_percentage: 69,
+         return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 3.5,
+            count: 1100,
+        },
+    },
+
+
+     {
+        id: '003',
+        image: 'Top-image/Top7.webp',
+        company: 'CUPID',
+        item_name: 'Cupid Woman Basic Printed Cotton Tees',
+        original_price: 995,
+        current_price: 349,
+        discount_percentage: 65,
+        return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 4.7,
+            count: 1900,
+        }
+    },
+
+     {
+        id: '004',
+        image: 'Top-image/Top8.jpg',
+        company: 'BEYOUNG',
+        item_name: 'Mambo Printed Oversized T-shirt for Women',
+        original_price: 1199,
+        current_price: 549,
+        discount_percentage: 54,
+        return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 4.7,
+            count: 1400,
+        }
+    },
+     {
+        id: '005',
+        image: 'Top-image/Top2.webp',
+        company: 'Tokyo Talkies',
+        item_name: 'Pink Shirt Collar Wrap Crop Top',
+        original_price: 949,
+        current_price: 294,
+        discount_percentage: 69,
+        return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 3.5,
+            count: 1100,
+        }
+    },
+
+     {
+        id: '006',
+        image: 'Top-image/Top3.webp',
+        company: 'CUPID',
+        item_name: 'Cotton Oversize Fit T-Shirt for Women & Girls ',
+        original_price: 1299,
+        current_price: 599,
+        discount_percentage: 55,
+        return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 3.9,
+            count: 1500,
+        }
+    },
+
+     {
+        id: '007',
+        image: 'Top-image/Top9.avif',
+        company: 'PINACOLDA',
+        item_name: 'Women Oversized Shirt with Spread Collar',
+        original_price: 1499,
+        current_price: 570,
+        discount_percentage: 62,
+        return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 4.5,
+            count: 1600,
+        }
+    },
+
+     {
+        id: '008',
+        image: 'Top-image/Top10.webp',
+        company: 'DKY',
+        item_name: 'Juliet Oversized T-Shirt',
+        original_price: 899,
+        current_price: 699,
+        discount_percentage: 22,
+        return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 4.1,
+            count: 1600,
+        }
+    },
+     {
+         id: '009',
+        image: 'Top-image/Top6.jpg',
+    company: 'LEVIS',
+      item_name: 'Women Cotton Solid Oversized Drop shoulder T-Shirt',
+       original_price: 1500,
+       current_price: 750,
+        discount_percentage: 50,
+        return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+           stars: 4.3,
+           count: 1400,
+        }
+     },
+      {
+        id: '010',
+        image: 'Top-image/Top4.png',
+        company: 'Lee',
+        item_name: `Women's All Purpose Print Button Down Shirt`,
+        original_price: 1999,
+        current_price: 1779,
+        discount_percentage: 11,
+        return_period: 14,
+        delivery_date: '01 May 2026',
+        rating: {
+            stars: 3.9,
+            count: 1800,
+        },
+    },
+    
+]
